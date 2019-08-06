89a133319477be1011b9bee4c0c89e49a4109369
Joshua Ross <jrossdiego@gmail.com>
2019-08-06 18:58:18 +0000

    added ajax to front end

---
+++ client/main.js
@@ -0,0 +1,21 @@
+
+
+$(document).ready( startApp );
+
+function startApp(){
+getData();
+}
+
+function getData(){
+  var settings = {
+    url: '../server/getnames.json',
+    method: 'GET',
+    dataType: 'json',
+    success: handleDataFromServer
+  }
+  $.ajax(settings)
+}
+
+function handleDataFromServer( response ){
+  console.log( response );
+}
