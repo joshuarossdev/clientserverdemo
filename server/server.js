beffb3e721e105cc58ba7b123f61e74307359c3c
Joshua Ross <jrossdiego@gmail.com>
2019-08-06 22:19:32 +0000

    store, read, and display data added data to server sent data through endpoints changed client address to correct server address put names on dom

--- server/server.js
+++ server/server.js
@@ -1,7 +1,39 @@
-[{
+const express = require('express');
+const server = express();
+const cors = require('cors');
+
+const names = [{
   "name": "Bob"
 }, {
   "name": "John"
 }, {
   "name": "Mary"
 }]
+
+const favFoods = [
+  {
+    "food": "mac n cheese",
+    "age": "4"
+  },
+  {
+    "food": "ketchup",
+    "age": "6"
+  },
+  {
+    "food": "sushi",
+    "age": "21"
+  }
+]
+
+server.use( cors() );
+
+server.get('/names', (request, response)=>{
+response.send(names)
+});
+
+server.get('/food', (request, response)=>{
+response.send(favFoods)
+});
+
+server.listen(3001)
+console.log('server listening');
