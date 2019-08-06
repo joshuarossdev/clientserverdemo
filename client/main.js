4951c30edc7ed687f4e9e035937326650a9c09ad
Joshua Ross <jrossdiego@gmail.com>
2019-08-06 19:54:26 +0000

    added test json data and ajax call

--- client/main.js
+++ client/main.js
@@ -8,7 +8,7 @@ getData();
 
 function getData(){
   var settings = {
-    url: '../server/getnames.json',
+    url: '../server/favoritefood.json',
     method: 'GET',
     dataType: 'json',
     success: handleDataFromServer
